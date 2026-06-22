810a5efd798f4b7fa5ed474e8e626caced2aa5c1
Abhishek Pal <119036895+abhishekk-09@users.noreply.github.com>
2026-06-22 23:56:28 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int minprice=prices[0],maxprofit=0;
+       for(int i=0;i<prices.size();i++)
+       {
+            minprice=min(minprice,prices[i]);
+            maxprofit=max(maxprofit,prices[i]-minprice);
+       }
+       return maxprofit;
+    }
+};
